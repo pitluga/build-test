65b55a5b162fcb0da50d63a942e6c0c3a1aba8f1
Anthony Pitluga <tony@pitluga-macbook-2.local>
2010-05-14 17:22:37 -0400

    regression test for local build

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,8 +2,11 @@ Project.configure do |project|
   project.build_command = case project.name
     when "failing_build" then "rake test:failing"
     when "passing_build" then "rake test:passing make_artifact"
+    when "local_build" then "rake test:passing"
     else raise "not found"
   end
 
-  project.elastic_build.pool = :testpool if project.respond_to? :elastic_build
+  if project.respond_to? :elastic_build
+    project.elastic_build.pool = :testpool unless project.name =~ /local/
+  end
 end
